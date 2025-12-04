8b5fd604969326286850c9fdc4383e0b2ebf40c7
dakshgoel2008 <goel.daksh2008@gmail.com>
2025-12-04 21:45:15 +0530

    Day 4

---
+++ 2025/Day 4/sol.py
@@ -0,0 +1,77 @@
+"""
+author: UG_BEAST
+"""
+
+
+def read_input():
+    with open("input.txt") as f:
+        return f.read().strip()
+
+
+def parse_lines(data):
+    parsed_data = []
+    for line in data.splitlines():
+        if not line:
+            continue
+
+        parsed_data.append(line)
+    return parsed_data
+
+
+def part1(grid):
+    result = 0
+    m = len(grid)
+    n = len(grid[0])
+    for i in range(m):
+        for j in range(n):
+            if grid[i][j] == "@":
+                cnt = 0
+                for dx in [-1, 0, 1]:
+                    for dy in [-1, 0, 1]:
+                        if dx == 0 and dy == 0:
+                            continue
+                        ni, nj = i + dx, j + dy
+                        if 0 <= ni < m and 0 <= nj < n and grid[ni][nj] == "@":
+                            cnt += 1
+                if cnt < 4:
+                    result += 1
+    return result
+
+
+def part2(grid):
+    result = 0
+    dkg = [[1 if ch == "@" else 0 for ch in row] for row in grid]
+    m = len(dkg)
+    n = len(dkg[0])
+    # parse the dkg grid until I can get some element with neighbors < 4
+    # and if I don't find any, I stop
+    flag = 1
+    while flag:
+        flag = 0
+        for i in range(m):
+            for j in range(n):
+                if dkg[i][j] == 1:
+                    cnt = 0
+                    for dx in [-1, 0, 1]:
+                        for dy in [-1, 0, 1]:
+                            if dx == 0 and dy == 0:
+                                continue
+                            ni, nj = i + dx, j + dy
+                            if 0 <= ni < m and 0 <= nj < n and dkg[ni][nj] == 1:
+                                cnt += 1
+                    if cnt < 4:
+                        dkg[i][j] = 0
+                        result += 1
+                        flag = 1
+    return result
+
+
+def main():
+    raw_input = read_input()
+    data = parse_lines(raw_input)
+    print("Part 1:", part1(data))
+    print("Part 2:", part2(data))
+
+
+if __name__ == "__main__":
+    main()
